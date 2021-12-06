e88abd717415e24304c0f8a1fdc8905aa7988abb
Antti Haapala <antti.haapala@anttipatterns.com>
2021-12-06 21:54:47 +0200

    day 6 solution

--- days/day06.py
+++ days/day06.py
@@ -1,18 +1,30 @@
 from helpers import *
 
 test_data = Data("""
+3,4,3,1,2
 """)
 
-test_case(1, test_data, ...)
-test_case(2, test_data, ...)
+test_case(1, test_data, 5934)
+test_case(2, test_data, 26984457539)
 
 
-def part1(d: Data, ans: Answers) -> None:
-    ...
+def part1_and_2(d: Data, ans: Answers) -> None:
+    initial = d.extract_ints
+    current = Counter(initial)
+
+    for _ in range(256):
+        new = Counter()
+        for k, v in current.items():
+            if k == 0:
+                new[6] += v
+                new[8] += v
+            else:
+                new[k - 1] += v
+        current = new
+        if _ == 79:
+            ans.part1 = sum(current.values())
+
+    ans.part2 = sum(current.values())
 
 
-def part2(d: Data, ans: Answers) -> None:
-    ...
-
-
-run([1], day=6, year=2021, submit=True)
+run([1, 2], day=6, year=2021, submit=True)
