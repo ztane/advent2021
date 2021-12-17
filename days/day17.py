247ec93f54f522b59b0987a470ca77aeb647ca1f
Antti Haapala <antti.haapala@anttipatterns.com>
2021-12-17 18:21:37 +0200

    less code for day 1

--- days/day17.py
+++ days/day17.py
@@ -11,19 +11,12 @@ test_case(2, test_data, 112)
 def part1_and_2(d: Input, ans: Answers) -> None:
     x1, x2, y1, y2 = d.parsed('target area: x=<int>..<int>, y=<int>..<int>')
 
-    bottom = min(y1, y2)
-    initial_speed = -bottom - 1
-
-    totals = 0
-    for j in interval(1, initial_speed):
-        totals += j
-
-    ans.part1 = totals
+    ans.part1 = (y1 + 1) * y1 // 2
 
     unique_hitting_vs = 0
-    for vx, vy in product(interval(1, x2 + 1), interval(bottom - 1, -bottom + 1)):
+    for vx, vy in product(interval(1, x2 + 1), interval(y1 - 1, -y1 + 1)):
         x = y = 0
-        while y >= bottom:
+        while y >= y1:
             x += vx
             y += vy
             if vx > 0:
